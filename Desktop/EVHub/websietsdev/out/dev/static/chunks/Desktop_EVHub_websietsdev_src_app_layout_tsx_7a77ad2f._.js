f57ff62f75051253865ccc0f13644688022ad8bf
Aviram Eldar <aviram@eldarvisual.com>
2026-07-22 17:59:52 +0300

    chore: remove stale development build output files

--- Desktop/EVHub/websietsdev/out/dev/static/chunks/Desktop_EVHub_websietsdev_src_app_layout_tsx_7a77ad2f._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__d8d9875c._.css",
-  "static/chunks/68532_next_dist_client_2d97c0ba._.js"
-],
-    source: "dynamic"
-});
--- Desktop/EVHub/websietsdev/out/dev/static/chunks/Desktop_EVHub_websietsdev_src_components_ScrollContent_tsx_54e2a55d._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/Desktop_EVHub_websietsdev_src_components_Hero_module_31f5ca50.css",
-  "static/chunks/Desktop_EVHub_websietsdev_src_components_4c6ea0e7._.js"
-],
-    source: "dynamic"
-});
